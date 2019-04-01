583344b84cd724114e37a44e73e2ca57c3b5beba
Quinten De Swaef <quintendeswaef@gmail.com>
2019-04-01 11:22:08 +0200

    Ethereum node monitor

--- src/main/kotlin/network/arkane/monitor/ethereummonitor/EthereumNodeHealthIndicator.kt
+++ src/main/kotlin/network/arkane/monitor/ethereummonitor/EthereumNodeHealthIndicator.kt
@@ -8,12 +8,10 @@ import org.web3j.protocol.Web3j
 import org.web3j.protocol.core.DefaultBlockParameterName
 import org.web3j.protocol.core.methods.response.EthBlock
 import reactor.core.publisher.Mono
-
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.temporal.ChronoUnit
-import java.util.Date
-import java.util.Optional
+import java.util.*
 
 @Component
 class EthereumNodeHealthIndicator(private val web3j: Web3j) : AbstractReactiveHealthIndicator() {
@@ -32,21 +30,22 @@ class EthereumNodeHealthIndicator(private val web3j: Web3j) : AbstractReactiveHe
     override fun doHealthCheck(builder: Health.Builder): Mono<Health> {
         try {
             val block = latestBlock
-            if (block.isPresent) {
-                val date = Date(block.get().block.timestamp.toLong() * 1000)
-                val blockTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault())
-                return if (blockTime.plus(10, ChronoUnit.MINUTES).isBefore(LocalDateTime.now(ZoneId.systemDefault()))) {
-                    Mono.just(builder.status(Status.DOWN)
-                            .withDetail("ethereumnode", "last block is older than 30 minutes")
-                            .build())
-                } else {
-                    Mono.just(builder.up().withDetail("ethereumnode", "latest block is " + block.get().block.number).build())
-                }
-            } else {
-                return Mono.just(
-                        builder.down()
-                                .withDetail("ethereumnode.down", "Unable to fetch status for ethereum node").build())
-            }
+            return block
+                    .map {
+                        val date = Date(block.get().block.timestamp.toLong() * 1000)
+                        val blockTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault())
+                        if (blockTime.plus(10, ChronoUnit.MINUTES).isBefore(LocalDateTime.now(ZoneId.systemDefault()))) {
+                            Mono.just(builder.status(Status.DOWN)
+                                    .withDetail("ethereumnode", "last block is older than 30 minutes.(" + blockTime.toString() + ", block: " + block.get().block.number + ")")
+                                    .build())
+                        } else {
+                            Mono.just(builder.up().withDetail("ethereumnode", "latest block is " + block.get().block.number).build())
+                        }
+                    }.orElse(
+                            Mono.just(
+                                    builder.down()
+                                            .withDetail("ethereumnode.down", "Unable to fetch status for ethereum node").build())
+                    )
         } catch (ex: Exception) {
             return Mono.just(
                     builder.down()
